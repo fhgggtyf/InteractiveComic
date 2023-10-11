f66d11705e7544042c2c9607d98aecb18af05f04
fhgggtyf <xjc18502795710@outlook.com>
2023-10-11 23:01:15 +0400

    com

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ let secondemily = document.getElementById('emily2');
 let currentEmilyTop2 = parseInt(secondemily.style.top);
 let currentEmilyLeft2 = parseInt(secondemily.style.left);
 
+let thirdPageElement = document.getElementById('thirdpage');
+let currentTop3 = parseInt(thirdPageElement.style.top);
+let thirdemily = document.getElementById('emily3');
+let currentEmilyTop3 = parseInt(thirdemily.style.top);
+let currentEmilyLeft3 = parseInt(thirdemily.style.left);
+
 document.addEventListener('scroll', scrollEvent);
 
 function scrollEvent() {
@@ -30,9 +36,15 @@ function scrollEvent() {
 	}
 	else if (sectionNum >= 5.78 && sectionNum <= 8) {
 		secondPageElement.style.top = (currentTop2 + (pos - 2.9 * height)) + 'px';
-		secondemily.style.top=(currentEmilyTop2/100*height + (pos - 2.35*height)-pos/5) + 'px';
+		secondemily.style.top=(currentEmilyTop2/100*height + (pos - 2.35*height)-pos/5+40) + 'px';
 		secondemily.style.left=(currentEmilyLeft2/100*width + (pos - 2.9*height)/5) + 'px';
 		secondemily.style.height = 270 - (pos - 2.9*height)/10 + 'px';
+	}
+	else if (sectionNum >= 10 && sectionNum <= 15) {
+		thirdPageElement.style.top = (currentTop3 + (pos - 5 * height)) + 'px';
+		thirdemily.style.top=(currentEmilyTop3/100*height + (pos - 3.33*height)-pos/3) + 'px';
+		thirdemily.style.left=(currentEmilyLeft3/100*width + (pos - 5*height)*1.06) + 'px';
+		thirdemily.style.height = 2700 - (pos - 5*height)*3.15 + 'px';
 	}
 
 }
