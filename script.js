9afae65312a821223b40e4fcec9406a21d837f10
fhgggtyf <xjc18502795710@outlook.com>
2023-10-11 20:16:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+function scrollDownAndUp(duration) {
+	const scrollHeight = document.documentElement.scrollHeight;
+	const windowHeight = window.innerHeight;
+	const scrollStep = (scrollHeight - windowHeight) / (duration / 2); // Half the duration for each direction
+	let currentScrollPosition = document.documentElement.scrollHeight;
+	let scrollingDown = false;
+
+	function scroll() {
+		if (scrollingDown && currentScrollPosition < scrollHeight - windowHeight) {
+			currentScrollPosition += scrollStep;
+			window.scrollTo(0, currentScrollPosition);
+			requestAnimationFrame(scroll);
+		} else if (!scrollingDown && currentScrollPosition > 0) {
+			currentScrollPosition -= scrollStep;
+			window.scrollTo(0, currentScrollPosition);
+			requestAnimationFrame(scroll);
+		} else {
+			// Change scrolling direction when reaching the bottom or top
+				return;
+
+		}
+	}
+
+	requestAnimationFrame(scroll);
+}
+
+// Call the scrollDownAndUp function when the page loads
+window.addEventListener('load', function () {
+	setTimeout(function() {
+		// Scroll to the bottom of the page
+		document.documentElement.scrollTop = document.documentElement.scrollHeight;
+		// Alternatively, you can use document.body.scrollTop for older browsers:
+		// document.body.scrollTop = document.body.scrollHeight;
+	  }, 0);
+	scrollDownAndUp(150);
+});
+
 let firstPageElement = document.getElementById('firstpage');
 let currentTop1 = parseInt(firstPageElement.style.top);
 
@@ -18,11 +55,11 @@ function scrollEvent() {
 	// parseInt() function used to ignore decimals
 	let sectionNum = (pos / height) * 2;
 
-	if (sectionNum >= 2 && sectionNum <= 4) {
+	if (sectionNum >= 1.98 && sectionNum <= 4) {
 		firstPageElement.style.top = (currentTop1 + (pos - height)) + 'px';
 	}
-	else if(sectionNum>=5.25 && sectionNum<=90){
-		secondPageElement.style.top = (currentTop1 + (pos - 2.75*height)) + 'px';
+	else if (sectionNum >= 5.25 && sectionNum <= 90) {
+		secondPageElement.style.top = (currentTop1 + (pos - 2.75 * height)) + 'px';
 	}
 
 }
